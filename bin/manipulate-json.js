497712d314c6542cc6d0f0ecc88258eed1245a1a
Wesley Bomar <wbomar@tacc.utexas.edu>
2020-12-11 11:58:16 -0600

    RT-18751: Auto-Clear "N/A" value for `link`s

--- bin/manipulate-json.js
+++ bin/manipulate-json.js
@@ -19,9 +19,14 @@ function format(data) {
     entry.authors = formatAuthors(entry.authors);
   });
 
-  // Some entries mus tnot be used
+  // TODO: Combine these tasks to reduce looping through array twice
+  // Some entries must not be used
   data['entries'] = data['entries'].filter( entry => {
     return shouldKeepEntry(entry);
+  });
+  // Some entries have values meant for humans, but not output
+  data['entries'] = data['entries'].map( entry => {
+    return clearHumanOnlyValues(entry);
   });
 
   return data;
@@ -47,17 +52,49 @@ function formatAuthors(authors) {
   return authorsString;
 }
 
+/** Clear undesirable values of specific properties in a given entry */
+function clearHumanOnlyValues(entry) {
+  // WARNING: Columns (keys) to check may change
+  const keysToCheck = [
+    'link'
+  ];
+  // WARNING: The negavtive values may increase
+  const negativeValues = [
+    'N/A'
+  ];
+
+  Object.keys(entry).map(function(key) {
+    const shouldCheck = keysToCheck.includes(key);
+
+    if (shouldCheck) {
+      const value = entry[key];
+
+      const shouldClear = negativeValues.some( negativeValue => {
+        return (value === negativeValue);
+      });
+
+      if (shouldClear) {
+        entry[key] = '';
+      }
+    }
+  });
+
+  return entry;
+}
+
 /** Whether to retain an entry in the formatted data */
 function shouldKeepEntry(entry) {
+  // WARNING: The negavtive values may increase
   const negativeValues = [
     "DON'T POST YET",
     'DONT POST YET'
   ];
 
   // WARNING: The column name may change
-  // WARNING: The possibly conditions may icnrease
-  shouldNotKeep = negativeValues.some( value => {
-    return (entry.ticket_or_date === value);
+  const shouldNotKeep = negativeValues.some( negativeValue => {
+    const value = entry.ticket_or_date;
+
+    return (value === negativeValue);
   });
 
   return ! shouldNotKeep;
